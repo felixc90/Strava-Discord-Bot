537f528801e1b19f98bf0414bd868bf4df9b275d
felixcao <felixcao37@gmail.com>
2021-12-29 16:44:42 +1100

    added demo slash commands

--- index.js
+++ index.js
@@ -10,6 +10,20 @@ client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
 })
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	} else if (commandName === 'user') {
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	}
+});
+
 client.on("messageCreate", msg => {
     let prefix = "+"
     if (!msg.content.startsWith(prefix)) {
